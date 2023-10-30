3ccd9e6c5dc2318cea9f7aa07eff24b0ddff8242
bharatchandran <bharatchandrankr@gmail.com>
2023-10-30 19:39:16 +0530

    mern infrastructure

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
+import './App.css';
+import AuthPage from '../AuthPage/AuthPage';
+import HomePage from '../HomePage/HomePage';
+
+import NavBar from '../../components/NavBar/NavBar';
+
+export default function App() {
+  const [user, setUser] = useState(getUser());
+
+  return (
+    <main className="App">
+      { user ?
+          <>
+            <NavBar user={user} setUser={setUser} />
+            <Routes>
+              {/* Route components in here */}
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </>
+          :
+          <AuthPage setUser={setUser} />
+      }
+    </main>
+  );
+}
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import NoteCard from "../../components/NoteCard/NoteCard"
+import { useState, useEffect } from "react"
+export default function HomePage() {
+  
+  return (
+    <>
+   
+    <h1>Home Page</h1>
+    </>
+  );
+}
